d6ef1afb5fb709beb1a2bcdfd716b5e98a76786d
accrane <austincrane@gmail.com>
2022-09-26 11:51:57 -0400

    add full width

---
+++ page-full.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			if ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
+			    <?php get_template_part("template-parts/flexslider");?>
+				<div class="wrapper cap clear-bottom">
+					<section class="col-full">
+						<header>
+							<h1><?php the_title();?></h1>
+						</header>
+						<div class="copy">
+							<?php the_content();?>
+						</div><!--.copy-->
+					</section><!--.col-2-->
+				</div><!--.wrapper-->
+			</article><!-- #post-## -->
+
+			<?php endif; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
